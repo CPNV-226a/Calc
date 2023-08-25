7514c5643115eac6f4e15464854fa1366e1fd48f
Nicolas Glassey <nicolas.glassey@cpnv.ch>
2023-08-25 10:38:13 +0200

    feat: addition

--- Program.cs
+++ Program.cs
@@ -4,11 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("***************************\r\n");
-            Console.Write("Enter the first operator : ");
+            Console.WriteLine("***********INPUT*****************");
+            Console.Write("Enter the first operande : ");
             string op1 = Console.ReadLine();
-            Console.WriteLine("First operator detected : " + op1);
-            Console.WriteLine("***************************\r\n");
+            Console.WriteLine("First operande detected : " + op1);
+
+            Console.Write("Enter the operator : ");
+            string oper = Console.ReadLine();
+            Console.WriteLine("Operator detected : " + oper);
+
+            Console.Write("Enter the second operande : ");
+            string op2 = Console.ReadLine();
+            Console.WriteLine("Second operande detected : " + op2);
+            Console.WriteLine();
+
+            Console.WriteLine("**********RESULT*****************");
+            int result = int.Parse(op1) + int.Parse(op2);
+            Console.WriteLine("Calculation is " + op1 + oper + op2 + " = " + result);
+            Console.WriteLine("*********************************");
         }
     }
 }
