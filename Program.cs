1b219bb3fceae628522a67e0f9bd60afed78824f
Nicolas Glassey <nicolas.glassey@cpnv.ch>
2023-08-25 11:20:15 +0200

    feat: four base operators

--- Program.cs
+++ Program.cs
@@ -2,26 +2,58 @@
 {
     internal class Program
     {
+        static private float op1;
+        static private char oper;
+        static private float op2;
+        static private float result;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("***********INPUT*****************");
-            Console.Write("Enter the first operande : ");
-            string op1 = Console.ReadLine();
-            Console.WriteLine("First operande detected : " + op1);
+            while(true)
+            {
+                Console.WriteLine("***********INPUT*****************");
+                Console.Write("Enter the first operande : ");
+                op1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("First operande detected : " + op1);
 
-            Console.Write("Enter the operator : ");
-            string oper = Console.ReadLine();
-            Console.WriteLine("Operator detected : " + oper);
+                Console.Write("Enter the operator : ");
+                oper = char.Parse(Console.ReadLine());
+                Console.WriteLine("Operator detected : " + oper);
 
-            Console.Write("Enter the second operande : ");
-            string op2 = Console.ReadLine();
-            Console.WriteLine("Second operande detected : " + op2);
-            Console.WriteLine();
+                Console.Write("Enter the second operande : ");
+                op2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Second operande detected : " + op2);
+                Console.WriteLine();
 
-            Console.WriteLine("**********RESULT*****************");
-            int result = int.Parse(op1) + int.Parse(op2);
-            Console.WriteLine("Calculation is " + op1 + oper + op2 + " = " + result);
-            Console.WriteLine("*********************************");
+                Console.WriteLine("**********RESULT*****************");
+                try
+                {
+                    result = Calculate(op1, oper, op2);
+                }
+                catch (NotImplementedException ex)
+                {
+                    Console.Write("This operator doesn't exist !");
+                }
+                Console.Clear();
+                Console.WriteLine("Calculation is " + op1 + oper + op2 + " = " + result);
+            }
+        }
+
+        private static float Calculate(float op1, char oper, float op2)
+        {
+            switch (oper) 
+            {
+                case '+':
+                    return op1 + op2;
+                case '-':
+                    return op1 - op2;
+                case '*':
+                    return op1 * op2;
+                case '/':
+                    return op1 / op2;
+                default:
+                    throw new NotImplementedException();
+               }
         }
     }
 }
